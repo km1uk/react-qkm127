32f0cd1e629782e5dbfe5f4bf1a331954ba95ca9
km1uk <keith@kgmm.co.uk>
2020-02-16 22:40:43 +0000

    1 file modified

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div><h1>hello world</h1><p>this is a paragraph</p></div>, document.getElementById('root'));
+//ReactDOM.render(<div><h1>hello world</h1><p>this is a paragraph</p></div>, document.getElementById('root'));
+
+ReactDOM.render(
+  <ul>
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+  </ul>,
+  document.getElementById('root')
+)
